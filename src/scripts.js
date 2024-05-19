dc39b43130a3ae99c33303991d4dfc97ebd29b2a
arufianz <alfianwashere@gmail.com>
2024-05-19 15:25:12 +0700

    commit?

---
+++ src/scripts.js
@@ -0,0 +1,12 @@
+function igLink() {
+  window.open("https://www.instagram.com/vestiazeta", "_blank");
+}
+function fbLink() {
+  window.open("https://www.facebook.com/VestiaZeta", "_blank");
+}
+function ytLink() {
+  window.open("https://www.youtube.com/@VestiaZeta", "_blank");
+}
+function xLink() {
+  window.open("https://www.twitter.com/vestiazeta", "_blank");
+}
